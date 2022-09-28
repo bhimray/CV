8da9c4a2b86a474ec9b731c63c2c8a2db118bab0
bilenray <raybimlendra@gmail.com>
2022-09-28 12:09:24 +1000

    changin-project-data

--- bilen-cv/src/components/Project/Project.jsx
+++ bilen-cv/src/components/Project/Project.jsx
@@ -50,17 +50,17 @@ const Project = () => {
           <div className='project-language'>
             <div className="project-language-description">
               <img src={jsIcon} className='language-icon'></img>
-              <div className='language-percent'>25%</div>
+              <div className='language-percent'>50.5%</div>
               <div className='language-name'>JS</div>
             </div>
             <div className="project-language-description">
               <img src={htmlIcon} className='language-icon'></img>
-              <div className='language-percent'>20%</div>
+              <div className='language-percent'>3.7%</div>
               <div className='language-name'>HTML</div>
             </div>
             <div className="project-language-description">
               <img src={cssIcon} className='language-icon'></img>
-              <div className='language-percent'>10%</div>
+              <div className='language-percent'>45.8%</div>
               <div className='language-name'>CSS</div>
             </div>
           </div>
@@ -79,17 +79,17 @@ const Project = () => {
           <div className='project-language'>
             <div className="project-language-description">
               <img src={jsIcon} className='language-icon'></img>
-              <div className='language-percent'>39%</div>
+              <div className='language-percent'>45.7%</div>
               <div className='language-name'>js</div>
             </div>
             <div className="project-language-description">
               <img src={htmlIcon} className='language-icon'></img>
-              <div className='language-percent'>20%</div>
+              <div className='language-percent'>4.5%</div>
               <div className='language-name'>HTML</div>
             </div>
             <div className="project-language-description">
               <img src={cssIcon} className='language-icon'></img>
-              <div className='language-percent'>20%</div>
+              <div className='language-percent'>40%</div>
               <div className='language-name'>CSS</div>
             </div>
           </div>
@@ -108,17 +108,17 @@ const Project = () => {
           <div className='project-language'>
             <div className="project-language-description">
               <img src={jsIcon} className='language-icon'></img>
-              <div className='language-percent'>40%</div>
+              <div className='language-percent'>53.5%</div>
               <div className='language-name'>JS</div>
             </div>
             <div className="project-language-description">
               <img src={htmlIcon} className='language-icon'></img>
-              <div className='language-percent'>20%</div>
+              <div className='language-percent'>4.2%</div>
               <div className='language-name'>HTML</div>
             </div>
             <div className="project-language-description">
               <img src={cssIcon} className='language-icon'></img>
-              <div className='language-percent'>30%</div>
+              <div className='language-percent'>42.3%</div>
               <div className='language-name'>CSS</div>
             </div>
           </div>
@@ -137,17 +137,17 @@ const Project = () => {
           <div className='project-language'>
             <div className="project-language-description">
               <img src={jsIcon} className='language-icon'></img>
-              <div className='language-percent'>50%</div>
+              <div className='language-percent'>71%</div>
               <div className='language-name'>JS</div>
             </div>
             <div className="project-language-description">
               <img src={htmlIcon} className='language-icon'></img>
-              <div className='language-percent'>60%</div>
+              <div className='language-percent'>7.6%</div>
               <div className='language-name'>HTML</div>
             </div>
             <div className="project-language-description">
               <img src={cssIcon} className='language-icon'></img>
-              <div className='language-percent'>10%</div>
+              <div className='language-percent'>20.5%</div>
               <div className='language-name'>CSS</div>
             </div>
           </div>
